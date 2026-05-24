220d992237c389c01db2ebb7e4d495961d1e0c7a
CHIRAG <lathashailu1976@gmail.com>
2026-05-24 21:12:29 +0530

    Updates

---
+++ server/assets/_tanstack-start-manifest_v-C1wtYaun.js
@@ -0,0 +1,4 @@
+const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/Chirag/OneDrive/Desktop/portfolio/Lovable/mixes/neo-show-main/neo-show-main/src/routes/__root.tsx", children: ["/"], preloads: ["/portfolio_001/assets/index-DTIOoHIc.js"] }, "/": { filePath: "C:/Users/Chirag/OneDrive/Desktop/portfolio/Lovable/mixes/neo-show-main/neo-show-main/src/routes/index.tsx", children: void 0, preloads: ["/portfolio_001/assets/index-DgMihk38.js"] } }, clientEntry: "/portfolio_001/assets/index-DTIOoHIc.js" });
+export {
+  tsrStartManifest
+};
---
+++ server/index.js
@@ -0,0 +1,5 @@
+import { w } from "./assets/worker-entry-BhLEo6j1.js";
+import "node:events";
+export {
+  w as default
+};
